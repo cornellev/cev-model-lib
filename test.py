3ec5ab9974a38d4923a77c4f2456cc2a4b3ba666
Myles Pasetsky <myles.pasetsky@gmail.com>
2024-11-03 01:23:02 -0500

    Add bicycle model to test environment

--- test.py
+++ test.py
@@ -1,40 +1,57 @@
-from models import KinematicBicycleModel
+from models import KinematicBicycleModel, DynamicBicycleModel
 from visualizer import PygameEngine, BicycleModelVisualizer
 import numpy as np
 import pygame
-
-
-model = KinematicBicycleModel()
-
-# print(model.state)
-
-# for i in range(10):
-#     u = np.array([ [1], [np.deg2rad(20)] ])
-#     model.update(u, 0.1)
-
-
-print(model.state)
+import time
 
 class KeyboardInputDevice:
-    def __init__(self):
+    def __init__(self, scale_up, scale_down):
         self.u = np.zeros((2, 1))
+
+        self.scale_up = scale_up
+        self.scale_down = scale_down
 
     def get_input(self):
         return self.u
 
     def update_with_keys(self, keys):
         # TODO: parameterize maximum controllable velocity
-        self.u[0,0] = 0.3 * (keys[pygame.K_UP] - keys[pygame.K_DOWN])
-        self.u[1,0] = np.deg2rad(30) * (keys[pygame.K_LEFT] - keys[pygame.K_RIGHT])
+        self.u[0,0] = self.scale_up * (keys[pygame.K_UP] - keys[pygame.K_DOWN])
+        self.u[1,0] = self.scale_down * (keys[pygame.K_LEFT] - keys[pygame.K_RIGHT])
 
 
-kb = KeyboardInputDevice()
+def get_environment(which):
+    eng = PygameEngine(800, 640)
+    last_time = time.time()
 
-eng = PygameEngine(800, 640)
-eng.add_visualizers([ BicycleModelVisualizer(model) ])
-eng.add_input_devices([ kb ])
+    if which == "kinematic_bicycle":
+        model = KinematicBicycleModel(0.15)
+        kb = KeyboardInputDevice(scale_up=0.3, scale_down=np.deg2rad(30))
+        eng.add_visualizers([ BicycleModelVisualizer(model) ])
+        eng.add_input_devices([ kb ])
 
-def fn():
-    model.update(kb.get_input(), 0.1)
+        def fn():
+            nonlocal last_time
+            curr_time = time.time()
+            model.update(kb.get_input(), curr_time - last_time)
+            last_time = curr_time
 
-eng.run(fn)
+        return lambda: eng.run(fn)
+    
+    elif which == "dynamic_bicycle":
+        model = DynamicBicycleModel(0.15, 4.54, static_friction=0.9, kinetic_friction=0.7)
+        kb = KeyboardInputDevice(scale_up=44.8, scale_down=np.deg2rad(30))
+        eng.add_visualizers([ BicycleModelVisualizer(model) ])
+        eng.add_input_devices([ kb ])
+
+        def fn():
+            print(model.state[2,0])
+            nonlocal last_time
+            curr_time = time.time()
+            model.update(kb.get_input(), curr_time - last_time)
+            last_time = curr_time
+
+
+        return lambda: eng.run(fn)
+        
+get_environment("dynamic_bicycle")()
